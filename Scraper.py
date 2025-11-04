12283cf726921c8fb96d474212cabbd94f424697
GUSTAVO RODRIGUES LOPES <gustavo.lopes1@alunos.sc.senac.br>
2025-11-04 19:43:05 -0300

    1.0.3

--- Scraper.py
+++ Scraper.py
@@ -6,4 +6,5 @@ def scrape_site(url):
     paragraphs = soup.find_all('p')
     text = "\n".join([para.get_text() for para in paragraphs])
     return text
+
 #print(scrape_site("https://www.jovemprogramador.com.br/")+scrape_site("https://www.jovemprogramador.com.br/apoiadores.php")+scrape_site("https://www.jovemprogramador.com.br/sobre.php")+scrape_site("https://www.jovemprogramador.com.br/patrocinadores.php")+scrape_site("https://www.jovemprogramador.com.br/parceiros.php")+scrape_site("https://www.jovemprogramador.com.br/duvidas.php")+scrape_site("https://www.jovemprogramador.com.br/queroserprofessor/"))
