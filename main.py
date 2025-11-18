33385f19ce39786248c62f6e5e23af597f681e8f
dumbemail696969 <lewdemail696969@gmail.com>
2025-11-18 19:46:33 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, jsonify
-from Openai import get_bot_response 
+from openai import get_bot_response 
 app = Flask(__name__)
 @app.route('/')
 def index():
